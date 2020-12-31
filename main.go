2f3e5164ce44aceee0d16a9026192a1ccf57426d
nozo-moto <nozomotoitech@gmail.com>
2020-12-31 17:58:26 +0900

    try to use perlin implement

--- main.go
+++ main.go
@@ -4,20 +4,35 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"math/rand"
 	"os"
 
 	_ "image/png"
+
+	"github.com/aquilax/go-perlin"
 )
 
 const (
-	WIDTH  = 1920
-	HEIGHT = 1080
+	//	WIDTH  = 1920
+	//	HEIGHT = 1080
+	WIDTH  = 100
+	HEIGHT = 100
 )
 
 func setField() []color.Color {
+	const (
+		alpha       = 1.
+		beta        = 1.
+		n           = 3
+		seed  int64 = 10000
+	)
+	p := perlin.NewPerlinRandSource(alpha, beta, n, rand.NewSource(seed))
 	field := make([]color.Color, WIDTH*HEIGHT)
-	for i := 0; i < len(field); i++ {
-		field[i] = color.RGBA{0, 0, 0, 255}
+	for x := 0; x < WIDTH; x++ {
+		for y := 0; y < HEIGHT; y++ {
+			noise := p.Noise2D(float64(x)/10, float64(y)/10)
+			field[y*HEIGHT+x] = color.RGBA{0, 0, 0, uint8(255 * noise)}
+		}
 	}
 	return field
 }
